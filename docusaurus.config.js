f0e1f9e6059192ff172d50d4881ec17351cb84bb
Hadrien <26697460+ketsapiwiq@users.noreply.github.com>
2023-02-07 17:29:04 +0100

    change docusaurus config for publishing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Formation DevOps',
+  
+
   tagline: '',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docker.uptime-formation.fr',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,15 +18,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Uptime-Formation', // Usually your GitHub org/user name.
+  projectName: 'supports-docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'fr',
+    locales: ['fr'],
   },
 
   presets: [
